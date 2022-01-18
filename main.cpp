56af61da641f5a33be6079a7f9f7b1e9f6301518
Abao <abaodoit@gmail.com>
2022-01-18 22:01:24 +0800

    process input

--- main.cpp
+++ main.cpp
@@ -5,31 +5,45 @@
 #include <iostream>
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
 
 int main(int argc, char *argv[]) {
-  glfwInit();
-  glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  auto window = glfwCreateWindow(500, 500, "Hello", nullptr, nullptr);
-  if (!window) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
+    glfwInit();
+    glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPPLE__
+    glfwInitHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
+    std::string windowTitle;
+    auto window = glfwCreateWindow(500, 500, "LearnOpenGL", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-  while (!glfwWindowShouldClose(window)) {
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-  glfwTerminate();
-  return 0;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2, 0.3, 0.3, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
